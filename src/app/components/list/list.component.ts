5dcbfc0bfe4622cf4dd47060c865ad2fbebabf78
Dejan <drveni92@gmail.com>
2024-04-08 16:00:47 +0200

    Added catalog service

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {ProductComponent} from "../product/product.component";
+import {Product} from "../../models/product";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [
+    ProductComponent,
+    NgForOf
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  @Input() products: Product[];
 
 }
