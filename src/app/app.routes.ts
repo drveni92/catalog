6f9cf333c377aab95956ec3c9b9c875c617c4f3d
Dejan <drveni92@gmail.com>
2024-04-08 09:34:37 +0200

    Added catalog component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+import {CatalogComponent} from "./components/catalog/catalog.component";
+
+export const routes: Routes = [
+  {path: '', redirectTo: '/catalog', pathMatch: "full"},
+  {path: '/catalog', component: CatalogComponent}
+];
